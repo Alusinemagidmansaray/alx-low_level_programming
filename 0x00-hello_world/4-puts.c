c4d07d5649f8a5c78ac40e23019baacec1b53fb1
Alusinemagidmansary <majidmansaray1998@gmail.com>
2023-09-07 03:56:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stadio.h>
+/**
+ * main - A program that prints a line iwth puts function
+ * Return: o (success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle);
+reture (o);
+}
