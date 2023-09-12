6ff7b129b876c2a0be0466613e05fde235c8d9a9
Alusinemagidmansary <majidmansaray1998@gmail.com>
2023-09-11 18:07:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stadio.h>
+#include <stdio.h>
 /**
- * main - A program that prints a line iwth puts function
- * Return: o (success)
+ * main - A program that prints a line with puts function
+ * Return: (success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle);
-reture (o);
+puts("\"programming is like building a multilingual puzzle");
+return (o);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that print a line using the printf function
- * Return: o (success)
+ * Return: always o (success)
  */
 
 int main(void)
 {
-	printf("with proper gramma, but the outcome is a piece of art,\n")
-	return (o);
-
+	printf ("with proper gramma, but the outcome is a piece of art,\n")
+	return(o);
+}	
