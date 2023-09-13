d1af019ed3e41b20379accf810e47c207b96f2da
Alusinemagidmansary <majidmansaray1998@gmail.com>
2023-09-13 11:09:43 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,10 @@
  * Return: 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
-{	
+{
 	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
 	{
 	return (1);
 	}
 	return (0);
-}	
+}
