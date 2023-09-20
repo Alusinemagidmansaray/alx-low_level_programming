9fadb86569f4dfdffa69c3d8c4ae3f6538ab7fbc
Alusinemagidmansary <majidmansaray1998@gmail.com>
2023-09-19 19:34:56 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,14 +3,14 @@
  * swap_int - this function swap two int
  * @a: first parameter
  * @a: second parameter
- *
+ * REturn: void
  */
+
 void swap_int(int *a, int *b);
 {
-int c;
-c = *a;
-*a = *b;
-*b = *c;
+	int c;
 
-
+	c = *a;
+	*a = *b;
+	*b = c;
 }
